4bb9fe464379898d8e1aa56b3d181a5324a0494a
Joanna <rajaaa92@gmail.com>
2020-06-05 19:34:19 +0200

    cors

--- src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
+++ src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
@@ -4,12 +4,20 @@ import com.okta.spring.boot.oauth.Okta
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http
                 .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .cors()
+                .configurationSource({ request-> CorsConfiguration().applyPermitDefaultValues() })
                 .and()
                 .oauth2ResourceServer().jwt();
 
