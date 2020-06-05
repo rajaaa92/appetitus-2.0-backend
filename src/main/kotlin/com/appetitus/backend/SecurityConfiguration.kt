3908626e5a98a4c1704633dcdc74b4c1c5dd2f2f
Joanna <rajaaa92@gmail.com>
2020-06-05 13:46:47 +0200

    add csrf

--- src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
+++ src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
@@ -4,12 +4,16 @@ import com.okta.spring.boot.oauth.Okta
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http
                 .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                 .oauth2ResourceServer().jwt();
 
