c1ec424b333d6f3eb240bd62a1ada662930844a3
Joanna <rajaaa92@gmail.com>
2020-06-05 20:03:25 +0200

    cors set origin

--- src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
+++ src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
@@ -1,11 +1,15 @@
 package com.appetitus.backend
 
 import com.okta.spring.boot.oauth.Okta
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
@@ -17,11 +21,20 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                 .cors()
-                .configurationSource({ request-> CorsConfiguration().applyPermitDefaultValues() })
                 .and()
                 .oauth2ResourceServer().jwt();
 
         // Send a 401 message to the browser (w/o this, you'll see a blank page)
         Okta.configureResourceServer401ResponseBody(http);
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf(System.getenv("HOST"))
+        configuration.allowedMethods = listOf("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
