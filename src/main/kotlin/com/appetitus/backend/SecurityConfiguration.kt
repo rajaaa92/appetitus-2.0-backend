b4d657b26f0c17cf86f839e6c1d6b0f5dbdb4893
Joanna <rajaaa92@gmail.com>
2020-06-05 20:16:35 +0200

    hopsa

--- src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
+++ src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
@@ -6,16 +6,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
-<<<<<<< HEAD
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import java.util.*
-<<<<<<< HEAD
-=======
->>>>>>> 3908626e5a98a4c1704633dcdc74b4c1c5dd2f2f
-=======
->>>>>>> c1ec424b333d6f3eb240bd62a1ada662930844a3
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
@@ -26,11 +20,8 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
-<<<<<<< HEAD
                 .cors()
                 .and()
-=======
->>>>>>> 3908626e5a98a4c1704633dcdc74b4c1c5dd2f2f
                 .oauth2ResourceServer().jwt();
 
         // Send a 401 message to the browser (w/o this, you'll see a blank page)
@@ -40,11 +31,7 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource? {
         val configuration = CorsConfiguration()
-<<<<<<< HEAD
         configuration.allowedOrigins = listOf(System.getenv("FRONTEND_HOST"))
-=======
-        configuration.allowedOrigins = listOf(System.getenv("HOST"))
->>>>>>> c1ec424b333d6f3eb240bd62a1ada662930844a3
         configuration.allowedMethods = listOf("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
