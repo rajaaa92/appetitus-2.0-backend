163e210a0f1b79b8b193c4c8f128f3d6a93b40a4
Joanna <rajaaa92@gmail.com>
2020-07-02 05:44:48 +0200

    add oauth2 issuer

--- src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
+++ src/main/kotlin/com/appetitus/backend/SecurityConfiguration.kt
@@ -1,15 +1,11 @@
 package com.appetitus.backend
 
 import com.okta.spring.boot.oauth.Okta
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import java.util.*
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
@@ -23,7 +19,7 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 .cors()
                 .configurationSource({ request-> CorsConfiguration().applyPermitDefaultValues() })
                 .and()
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt()
 
         // Send a 401 message to the browser (w/o this, you'll see a blank page)
         Okta.configureResourceServer401ResponseBody(http);
